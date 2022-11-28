ace09969148ba42f9195134b1c4c92ba8ac9357e
Damien Nelson <dhnisaman@gmail.com>
2022-11-28 13:38:57 -0500

    commit tree d5b24a1b405c5d7c93954c2125483120d9104f91

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(arr){
+    const obj = arr.find(item => item.result === "W")
+    return (typeof(obj) === "object") ? obj.year : undefined
+  }
+  
